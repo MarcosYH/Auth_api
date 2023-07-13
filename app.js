66cf235404ef83f7fef01937d4f93d9a1840da41
MarcosYH <marcosdeimos18@gmail.com>
2023-07-13 21:39:18 +0200

    test structure

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const User = require("./db/userModel");
-const jwt = require("jsonwebtoken");
-const { google } = require("googleapis");
-const { OAuth2Client } = require("google-auth-library");
-const Cookies = require("universal-cookie");
 // const auth = require("./auth");
 const dotenv = require("dotenv");
 
@@ -49,108 +44,108 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", userRoutes);
 
-app.post("/auth/google", async function (req, res, next) {
-  const redirectURL = "https://auth-api-adk2.onrender.com/auth/google/callback";
-  const GOOGLE_CLIENT_ID =
-    "881382327006-7mbuorq3in23d3so4n6n6l1n4a4ni5ga.apps.googleusercontent.com";
-  const GOOGLE_CLIENT_SECRET = "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";
+// app.post("/auth/google", async function (req, res, next) {
+//   const redirectURL = "https://auth-api-adk2.onrender.com/auth/google/callback";
+//   const GOOGLE_CLIENT_ID =
+//     "881382327006-7mbuorq3in23d3so4n6n6l1n4a4ni5ga.apps.googleusercontent.com";
+//   const GOOGLE_CLIENT_SECRET = "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";
 
-  const oAuth2Client = new OAuth2Client(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    redirectURL
-  );
+//   const oAuth2Client = new OAuth2Client(
+//     GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET,
+//     redirectURL
+//   );
 
-  // Generate the url that will be used for the consent dialog.
-  const authorizeUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.profile  openid ",
-      "https://www.googleapis.com/auth/userinfo.email",
-    ],
-    prompt: "consent",
-  });
+//   // Generate the url that will be used for the consent dialog.
+//   const authorizeUrl = oAuth2Client.generateAuthUrl({
+//     access_type: "offline",
+//     scope: [
+//       "https://www.googleapis.com/auth/userinfo.profile  openid ",
+//       "https://www.googleapis.com/auth/userinfo.email",
+//     ],
+//     prompt: "consent",
+//   });
 
-  res.json({ url: authorizeUrl });
-});
+//   res.json({ url: authorizeUrl });
+// });
 
-app.get("/auth/google/callback", async function (req, res, next) {
-  const code = req.query.code;
+// app.get("/auth/google/callback", async function (req, res, next) {
+//   const code = req.query.code;
 
-  try {
-    const redirectURL =
-      "https://auth-api-adk2.onrender.com/auth/google/callback";
-    const GOOGLE_CLIENT_ID =
-      "881382327006-7mbuorq3in23d3so4n6n6l1n4a4ni5ga.apps.googleusercontent.com";
-    const GOOGLE_CLIENT_SECRET = "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";
-    const oAuth2Client = new OAuth2Client(
-      GOOGLE_CLIENT_ID,
-      GOOGLE_CLIENT_SECRET,
-      redirectURL
-    );
+//   try {
+//     const redirectURL =
+//       "https://auth-api-adk2.onrender.com/auth/google/callback";
+//     const GOOGLE_CLIENT_ID =
+//       "881382327006-7mbuorq3in23d3so4n6n6l1n4a4ni5ga.apps.googleusercontent.com";
+//     const GOOGLE_CLIENT_SECRET = "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";
+//     const oAuth2Client = new OAuth2Client(
+//       GOOGLE_CLIENT_ID,
+//       GOOGLE_CLIENT_SECRET,
+//       redirectURL
+//     );
 
-    const { tokens } = await oAuth2Client.getToken(code);
-    await oAuth2Client.setCredentials(tokens);
-    console.info("Tokens acquired.");
+//     const { tokens } = await oAuth2Client.getToken(code);
+//     await oAuth2Client.setCredentials(tokens);
+//     console.info("Tokens acquired.");
 
-    // j'utilise l'API Google pour obtenir les informations de l'utilisateur
-    const oauth2 = google.oauth2({
-      auth: oAuth2Client,
-      version: "v2",
-    });
-    const { data } = await oauth2.userinfo.get();
-    const { id, name, email } = data;
-    const cookies = new Cookies(req, res);
-    // Utilisez le nom et l'email de l'utilisateur pour effectuer des opérations
-    // telles que l'enregistrement dans la base de données
+//     // j'utilise l'API Google pour obtenir les informations de l'utilisateur
+//     const oauth2 = google.oauth2({
+//       auth: oAuth2Client,
+//       version: "v2",
+//     });
+//     const { data } = await oauth2.userinfo.get();
+//     const { id, name, email } = data;
+//     const cookies = new Cookies(req, res);
+//     // Utilisez le nom et l'email de l'utilisateur pour effectuer des opérations
+//     // telles que l'enregistrement dans la base de données
 
-    const user = new User({
-      name: name,
-      email: email,
-      googleID: id,
-    });
-    // save the new user
-    user
-      .save()
-      // return success if the new user is added to the database successfully
-      .then((result) => {
-        //   create JWT token
-        console.log(result, "User Created Successfully");
-      });
-    const token = jwt.sign(
-      {
-        userId: user._id,
-        userEmail: user.email,
-      },
-      "RANDOM-TOKEN",
-      { expiresIn: "24h" }
-    )
-    user.token = token;
-    res.cookie('auth_token', token, { httpOnly: true });
-    // user.token=token;
-    res.cookie("TOKEN", token);
-    res.cookie("EMAIL", user.email);
+//     const user = new User({
+//       name: name,
+//       email: email,
+//       googleID: id,
+//     });
+//     // save the new user
+//     user
+//       .save()
+//       // return success if the new user is added to the database successfully
+//       .then((result) => {
+//         //   create JWT token
+//         console.log(result, "User Created Successfully");
+//       });
+//     const token = jwt.sign(
+//       {
+//         userId: user._id,
+//         userEmail: user.email,
+//       },
+//       "RANDOM-TOKEN",
+//       { expiresIn: "24h" }
+//     )
+//     user.token = token;
+//     res.cookie('auth_token', token, { httpOnly: true });
+//     // user.token=token;
+//     res.cookie("TOKEN", token);
+//     res.cookie("EMAIL", user.email);
     
 
-    // Stocker le token et l'email de l'utilisateur dans les cookies
-    cookies.set("TOKEN", token, { path: "/" });
-    cookies.set("EMAIL", user.email, { path: "/" });
+//     // Stocker le token et l'email de l'utilisateur dans les cookies
+//     cookies.set("TOKEN", token, { path: "/" });
+//     cookies.set("EMAIL", user.email, { path: "/" });
 
-    // // catch error if the new user wasn't added successfully to the database
-    // .catch((error) => {
-    //   res.status(500).send({
-    //     message: "Error creating user",
-    //     error,
-    //   });
-    //   console.log(error, "Error creating user");
-    // });
-    console.log(data);
-    res.redirect(`https://authentification-eight.vercel.app/welcome/${token}`);
-    // res.redirect(`http://localhost:3001/welcome?token=${encodeURIComponent(token)}`);
-  } catch (err) {
-    console.log("Error logging in with OAuth2 user", err);
-    res.redirect("https://authentification-eight.vercel.app/error");
-  }
-});
+//     // // catch error if the new user wasn't added successfully to the database
+//     // .catch((error) => {
+//     //   res.status(500).send({
+//     //     message: "Error creating user",
+//     //     error,
+//     //   });
+//     //   console.log(error, "Error creating user");
+//     // });
+//     console.log(data);
+//     res.redirect(`https://authentification-eight.vercel.app/welcome/${token}`);
+//     // res.redirect(`http://localhost:3001/welcome?token=${encodeURIComponent(token)}`);
+//   } catch (err) {
+//     console.log("Error logging in with OAuth2 user", err);
+//     res.redirect("https://authentification-eight.vercel.app/error");
+//   }
+// });
 
 module.exports = app;
